12f0c0d6e000d77686312c2647bbb1193cf8a3af
Alin RADU <cristian.alin.radu@reea.net>
2022-11-14 09:58:56 +0200

    create Term and Conditions tooltip component and tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
+import { SummaryForm } from './pages/summary/SummaryForm';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      App
+      <div>
+        <SummaryForm />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/summary/tests/SummaryForm.test.jsx
+++ client/src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,8 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { SummaryForm } from '../SummaryForm';
 
 describe('SummaryForm', () => {
-  test('Initial conditions', () => {
+  test('initial conditions', () => {
     render(<SummaryForm />);
     const checkbox = screen.getByRole('checkbox', {
       name: /terms and conditions/i,
@@ -13,17 +15,43 @@ describe('SummaryForm', () => {
     expect(confirmButton).toBeDisabled();
   });
 
-  test('Checkbox enables button on first click and disables on second click', () => {
-    render(<SummaryForm />);
-    const checkbox = screen.getByRole('checkbox', {
-      name: /terms and conditions/i,
+  describe('Checkbox', () => {
+    test('checkbox enables button on first click and disables on second click', async () => {
+      const user = userEvent.setup();
+
+      render(<SummaryForm />);
+      const checkbox = screen.getByRole('checkbox', {
+        name: /terms and conditions/i,
+      });
+      const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+
+      await user.click(checkbox);
+      expect(confirmButton).toBeEnabled();
+
+      await user.click(checkbox);
+      expect(confirmButton).toBeDisabled();
     });
-    const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+  });
 
-    fireEvent.click(checkbox);
-    expect(confirmButton).toBeEnabled();
+  describe('Popover', () => {
+    test('popover responds to hover', async () => {
+      const user = userEvent.setup();
+      render(<SummaryForm />);
 
-    fireEvent.click(checkbox);
-    expect(confirmButton).toBeDisabled();
+      // popover starts hidden
+      const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+      expect(nullPopover).not.toBeInTheDocument();
+
+      // popover apperars on monseover of checkbox label
+      const termsAndConditions = screen.getByText(/terms and conditions/i);
+      await user.hover(termsAndConditions);
+
+      const popover = screen.getByText(/no ice cream will actually be delivered/i);
+      expect(popover).toBeInTheDocument();
+
+      // popover disappears when we mouse out
+      await user.unhover(termsAndConditions);
+      expect(popover).not.toBeInTheDocument();
+    });
   });
 });
